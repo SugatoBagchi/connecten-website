25f0bb2cba714f8ac89f9c074237eab59f93141a
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2023-03-11 01:35:49 +0530

    firebase working

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,35 +6,19 @@ import coin from "../assets/coin.png";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import { useUserAuth } from "../context/UserAuthContext";
 import Connection from "../components/Connection";
+import { useNavigate } from "react-router";
 
 const Profile = () => {
-  const profileJSON = {
-    name: "Varun Doshi",
-    designation: "Blockchain",
-    bio: "Awesome blockchain developer",
-    email: "doshivarun@gmail.com",
-    imageURL: "imageURL",
-    connectedList: [2, 7, 1, 6, 3, 9],
-    github: "github",
-    linkedin: "linkedin",
-    twitter: "twitter",
-    portfolio: "portfolio",
-    isPrivate: false,
-    coins: 1000,
-  };
-
   const { user } = useUserAuth();
   const [dbData, setDbData] = useState({});
   const db = getFirestore();
+  const navigate = useNavigate();
 
-  console.log(user?.uid);
   useEffect(() => {
     const getDb = async () => {
       const docRef = doc(db, "users", user?.uid);
       try {
-        console.log(user?.uid);
         const docSnap = await getDoc(docRef);
-        const userData = docSnap.data();
         setDbData(docSnap.data());
       } catch (error) {
         console.log(error);
@@ -49,19 +33,19 @@ const Profile = () => {
     <div className="font-roboto  ">
       <div className="flex justify-between items-center bg-[#EFF5F5] pt-24 pb-8">
         <div className="px-8">
-          <p className=" text-5xl font-bold">{profileJSON.name}</p>
+          <p className=" text-5xl font-bold">{dbData.name}</p>
           <div className="flex justify-evenly items-center gap-6 text-gray-500">
             <div className="flex items-center gap-2 text-xl">
               <FaSuitcase />
-              {profileJSON.designation}
+              {dbData.designation}
             </div>
             <div className="flex items-center gap-2 text-xl">
               <MdEmail />
-              {profileJSON.email}
+              {dbData.email}
             </div>
             <div className="flex items-center gap-2 text-xl">
               <img className="w-8 bg-gray-500 rounded-full" src={coin} alt="" />
-              {profileJSON.coins}
+              {dbData.coins}
             </div>
           </div>
         </div>
@@ -83,14 +67,16 @@ const Profile = () => {
           </div>
           <div className="text-white px-2 cursor-pointer flex items-center text-2xl border-2 bg-[#609966] justify-center rounded-md">
             <BsCashStack />
-            <button className="px-2">Buy</button>
+            <button className="px-2" onClick={() => navigate("/buy")}>
+              Buy
+            </button>
           </div>
         </div>
       </div>
 
       {/* Connected ppl */}
       <div className="flex flex-col py-10 px-8 ">
-        {profileJSON.connectedList.map((connect) => {
+        {dbData.connectedList?.map((connect) => {
           return <Connection key={connect} id={connect} />;
         })}
       </div>
