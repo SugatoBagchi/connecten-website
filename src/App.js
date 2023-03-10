96d2c5533ec06c70b7898a593fcdb0bdf59f3126
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2023-03-10 14:32:46 +0530

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Login from './pages/Login.js';
+import { UserAuthProvider } from './context/UserAuthContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='flex justify-center'>App</div>
-  )
+    <UserAuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </UserAuthProvider>
+  );
 }
 
 export default App
